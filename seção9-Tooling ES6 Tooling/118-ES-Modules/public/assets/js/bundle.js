a0ebfa13501f5d41c8b55cdd823672880d741d10
Danilo Henrique <danilomhenrique311@gmail.com>
2022-12-21 12:15:36 -0300

    ""

---
+++ seção9-Tooling ES6 Tooling/118-ES-Modules/public/assets/js/bundle.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";console.log(200),console.log("Danilo","Marques",18,8)})();
+//# sourceMappingURL=bundle.js.map
---
+++ seção9-Tooling ES6 Tooling/118-ES-Modules/src/module1.js
@@ -0,0 +1,10 @@
+const nome = 'Danilo';
+const sobrenome = 'Marques';
+const idade = 18;
+
+function soma(x, y) {
+    return x + y;
+}
+export default (x, y) => x * y;
+
+export { nome, sobrenome, idade, soma }
